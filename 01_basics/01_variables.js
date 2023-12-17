6a58bfe04e1f490bc27d67031327f9718b0a344a
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-17 18:49:09 +0530

    add basics of variable code

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,34 @@
+// const accountId = 144553
+// let accountEmail = "akash@google.com"
+// var accountPassword = "12345"
+// accountCity = "chalisgaon"
+// let accountState;
+
+// accountId = 2 // not allowed
+
+
+// accountEmail = "ak@hc.com" 
+// accountPassword = "21212121"
+// accountCity = "Bengaluru"
+
+// console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// const first = 1000;
+// let second = "Akash"
+// var third = "199119"
+
+
+// accountType = "saving"
+
+// first = "19199"
+// second = "sagar"
+
+// console.table([first, second, third, accountType])
