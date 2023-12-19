037987d0ea3f7df01a2134fa54cfd95282aa2ded
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-19 16:00:52 +0530

    ...

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -9,7 +9,7 @@ const dc_heros = ["superman", "flash", "batman"]
 // const allHeros = marvel_heros.concat(dc_heros)
 // console.log(allHeros);
 
-const all_new_heros = [...marvel_heros, ...dc_heros]
+// const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
 
@@ -18,14 +18,14 @@ const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 const real_another_array = another_array.flat(Infinity)
 console.log(real_another_array);
 
-
-
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Akash"))
+console.log(Array.from("Akash"))
+console.log(Array.from({name: "Akash"})) // interesting
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
 console.log(Array.of(score1, score2, score3));
+
+console.log(Array.from(score1,score2,score3));
--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -6,13 +6,13 @@
 const mySym = Symbol("key1")
 
 
-const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+const JsUser = { 
+    name: "Akash",
+    "full name": "Akash Choudhary",
     [mySym]: "mykey1",
     age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    location: "Chalisgaon",
+    email: "Akash@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,17 +22,18 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+// JsUser.email = "akash@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+// JsUser.email = "akash@microsoft.com"
 // console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
-}
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// console.log(JsUser.greeting());
+// console.log(JsUser.greetingTwo());
+
