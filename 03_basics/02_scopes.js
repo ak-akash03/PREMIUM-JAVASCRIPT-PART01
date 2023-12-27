a5fbaef43d9a5a6451e344bbb5f31dd3dbe73184
Akash Rajendra Rathod <ak.akashrathod03@gmail.com>
2023-12-27 14:03:59 +0530

    ...

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,58 +1,61 @@
-//var c = 300
-let a = 300
-if (true) {
-    let a = 10
-    const b = 20
-    // console.log("INNER: ", a);
+// //var c = 300
+// let a = 300
+// if (true) {
+//     let a = 10
+//     const b = 20
+//     // console.log("INNER: ", a);
     
-}
+// }
 
 
 
-// console.log(a);
-// console.log(b);
-// console.log(c);
+// // console.log(a);
+// // console.log(b);
+// // console.log(c);
 
 
-function one(){
-    const username = "hitesh"
+// function one(){
+//     const username = "hitesh"
 
-    function two(){
-        const website = "youtube"
-        console.log(username);
-    }
-    // console.log(website);
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     // console.log(website);
 
-     two()
+//      two()
 
-}
+// }
 
-// one()
+// // one()
 
-if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
-        const website = " youtube"
-        // console.log(username + website);
-    }
-    // console.log(website);
-}
+// if (true) {
+//     const username = "hitesh"
+//     if (username === "hitesh") {
+//         const website = " youtube"
+//         // console.log(username + website);
+//     }
+//     // console.log(website);
+// }
 
-// console.log(username);
-
-
-// ++++++++++++++++++ interesting ++++++++++++++++++
+// // console.log(username);
 
 
-console.log(addone(5))
-
-function addone(num){
-    return num + 1
-}
+// // ++++++++++++++++++ interesting ++++++++++++++++++
 
 
+// console.log(addone(5))
 
-addTwo(5)
-const addTwo = function(num){
-    return num + 2
-}
+// function addone(num){
+//     return num + 1
+// }
+
+
+
+// addTwo(5)
+// const addTwo = function(num){
+//     return num + 2
+// }
+
+
+
