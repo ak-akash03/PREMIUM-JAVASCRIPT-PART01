ffe8389a0211160157e90ee4600b8bb8eba0000f
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-21 15:55:28 +0530

    add and commit 14_filter

---
+++ 14_map_reduse_filter/01.js
@@ -0,0 +1,56 @@
+console.log("javascript demo");
+
+const myNums = [1,2,3,4,5,6]
+
+// const newNum = myNums.filter( (num) => num > 3)
+// console.log(newNum);
+
+
+// const newNum = []
+// myNums.forEach((num) => {
+//     if (num > 3) {
+//         newNum.push(num)
+//     }
+// })
+
+// console.log(newNum);
+
+const Men = [
+    {
+        Name : "Akash",
+        Age : 20,
+        Course : "BCA"
+    },
+    {
+        Name : "Laxman",
+        Age : 21,
+        Course : "BCA"
+    },
+    {
+        Name : "Rahul",
+        Age : 22,
+        Course : "BCA"
+    },
+    {
+        Name : "Yogesh",
+        Age : 19,
+        Course : "BTECH"
+    }    
+]
+
+// Men.forEach((N) =>{
+//     console.log(`Moy Name Is ${N.Name} I'm ${N.Age} Year Old And I'm Currantly Lerning ${N.Course}` );
+// }) 
+
+// const userMen = Men.filter((bk) => {
+//     return bk.Course === "BCA"
+// })
+
+// console.log(userMen);
+
+
+// const userMen = Men.filter((bk) => {
+//     return bk.Age === 19
+// })
+
+// console.log(userMen);
