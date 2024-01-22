801a7555dea861f66892910ddeb28e72e3215a57
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-22 18:26:05 +0530

    update file

---
+++ 15_apnaCollege/02.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn');
+
+// btn.onclick = function () {
+//     alert("button was clicked : ")
+// }
+
+// btn.addEventListener("click",()=>{
+//     alert("button was clicked : ........")
+// })
+
+
