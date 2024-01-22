d55db9bed8b077685cb6d5b3e36575de56a274eb
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-22 18:56:50 +0530

    update file

--- 15_apnaCollege/02.js
+++ 15_apnaCollege/02.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.btn');
 
 // btn.onclick = function () {
 //     alert("button was clicked : ")
@@ -9,3 +9,14 @@ const btn = document.querySelector('.btn');
 // })
 
 
+// const button = document.querySelector("button")
+// const h3 = document.querySelector("h3")
+// const div = document.querySelector("div")
+
+document.getElementsByTagName('button')
+
+
+// button.addEventListener("click",GenRandomcolor)
+
+const num = Math.floor(Math.random() * 255)
+console.log(num);
---
+++ 15_apnaCollege/03.js
@@ -0,0 +1,22 @@
+// console.log("Hello Ak");
+
+// const button = document.querySelector('button')
+// const h3 = document.querySelector('h3')
+// const div = document.querySelector('div')
+
+
+// function genRandomColor() {
+//     const red = Math.floor(Math.random() * 255)
+//     const green = Math.floor(Math.random() * 255)
+//     const blue = Math.floor(Math.random() * 255)
+
+//     console.log("clicked");
+//     const newcolor = `rgb(${red},${green},${blue})`
+//     console.log(newcolor);
+
+//     h3.innerHTML = newcolor
+//     div.style.backgroundColor = newcolor
+    
+// }
+
+// button.addEventListener("click", genRandomColor)
