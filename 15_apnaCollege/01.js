e66cf477f155c0d25a40c2693086cb3a741c0cca
Mr. Akash Rathod <ak.akashrathod03@gmail.com>
2024-01-22 15:36:05 +0530

    15_apnacollage file commited :

---
+++ 15_apnaCollege/01.js
@@ -0,0 +1,46 @@
+console.log("Apna collage Lecture : ");
+
+// const arr = [1,2,3,4,5,6]
+
+// const print = (el) => {
+//     console.log(el);
+// }
+
+// arr.forEach(print)
+
+// arr.forEach((el) => console.log(el))
+
+// let arr = [
+//   { name: "Akash", mark: 43 },
+//   { name: "sagar", mark: 33 },
+//   { name: "rathod", mark: 63 },
+// ];
+
+// arr.forEach((el) => {
+//     console.log(`name ${el.name} and mark ${el.mark}`);
+// })
+
+// ----------- Map ----------------
+
+
+// const arr = [1,2,3,4,5];
+
+// const squre = arr.map((el) => {
+    //     return el*el
+    // })
+    
+    
+    // console.log(squre);
+    
+    
+    // let arr = [1,2,3,4,5];
+
+    // let ans = arr.filter((el) => {
+    //     return el % 2 == 0;
+    // })
+
+
+    // console.log(ans);
+
+
+    
